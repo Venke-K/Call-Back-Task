8a3ceca760751386b84123c500f200ba6f76987f
venkatjp62@gmail.com <venkatjp62@gmail.com>
2024-05-18 20:18:50 +0530

    commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let values = document.createElement('H1')
+document.body.append(values);
+
+setTimeout(()=>{
+    values.innerText = "10"
+    setTimeout(()=>{
+        values.innerText ="9"
+        setTimeout(()=>{
+            values.innerText ="8"
+            setTimeout(()=>{
+                values.innerText ="7"
+                setTimeout(()=>{
+                    values.innerText ="6"
+                    setTimeout(()=>{
+                        values.innerText ="5"
+                        setTimeout(()=>{
+                            values.innerText ="4"
+                            setTimeout(()=>{
+                                values.innerText ="3"
+                                setTimeout(()=>{
+                                    values.innerText ="2"
+                                    setTimeout(()=>{
+                                        values.innerText ="1"
+                                        setTimeout(()=>{
+                                            values.innerText ="Happy Independence Day !! "
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+             
